01dc288455dfe5ce7633868bf6c73dce41d7f696
Isbulla <isbullamlm@gmail.com>
2019-08-04 17:00:53 +0530

    Staring out

---
+++ WeatherAPI/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WeatherAPI.Controllers
+{
+    public class TestController : ApiController
+    {
+        [HttpGet, Route("api/Test")]
+        public IHttpActionResult Test()
+        {
+            return Ok("Test");
+        }
+    }
+}
